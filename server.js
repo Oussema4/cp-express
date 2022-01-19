f9a89ba68a34513e21df3745e20ff473e494e2a4
unknown <bomix20@gmail.com>
2022-01-19 11:06:44 +0100

    1

---
+++ server.js
@@ -0,0 +1,41 @@
+const express=require("express")
+
+const app =express()
+
+const d=new Date();
+const breaktime=(d.getHours()>17)||(d.getHours()<9)||(d.getDay()>5);
+
+const middleware=(req,res,next)=>{
+    breaktime?res.send("SORRY WE'RE CLOSED"):
+next();
+
+}
+app.use(middleware);
+
+
+
+
+
+app.get("/",(req,res)=>{
+   
+    res.sendFile(__dirname+"/Home.html")
+    
+    
+    });
+
+app.get("/Ourservices",(req,res)=>{
+
+    res.sendFile(__dirname+"/Ourservices.html")
+})
+
+
+app.get("/Contactus",(req,res)=>{
+
+    res.sendFile(__dirname+"/Contactus.html")
+})
+
+
+
+const port=2020
+
+app.listen(port,()=>console.log(`done${port}`))
