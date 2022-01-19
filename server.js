e93a4de23458e23805f6846339fbf6817d2b99c7
unknown <bomix20@gmail.com>
2022-01-19 14:23:56 +0100

    x

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express=require("express")
 const app =express()
 
 const d=new Date();
-const breaktime=(d.getHours()>17)||(d.getHours()<9)||(d.getDay()>5);
+const breaktime=(d.getHours()>17)||(d.getHours()<9)||(d.getDay()==6)||(d.getDay()==0);
 
 const middleware=(req,res,next)=>{
     breaktime?res.send("SORRY WE'RE CLOSED"):
